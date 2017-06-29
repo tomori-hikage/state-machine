142486f934949da9246e01a2783fe3437ad21dec
tomori_aki <tomori.aki2961@gmail.com>
2017-06-30 00:03:47 +0900

    StateMachineスクリプトの追加

---
+++ state-machine/Assets/Scripts/Framework/AI/State.cs
@@ -0,0 +1,154 @@
+﻿using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+using HC.UniRxCustom;
+
+
+/*
+ * このStateクラスを継承して各種ステートを定義する
+ * 
+ * 
+ * 各種処理はBegin,End,Update,LateUpdateのストリームを利用して記述する
+ * 
+ * ex)
+ *      BeginStream.Subscribe(_=>Debug.Log("State begin"));
+ *      EndStream.Subscribe(_=>Debug.Log("State end"));
+ *      UpdateStream.Subscribe(_=>Debug.Log("State update"));
+ *      ...
+ *      
+ * 他ステートへの遷移はTransition関数を使う
+ * ex)
+ *      UpdateStream.Where(_=>Input.GetMouseButtonDown(0))
+ *                      .Subscribe(_=>Transition(typeof(OtherState)));
+ *                      
+ * ジェネリック版)
+ *      UpdateStream.Where(_=>Input.GetMouseButtonDown(0))
+ *                      .Subscribe(_=>Transition<OtherState>());
+ */
+
+
+namespace HC.AI
+{
+    /// <summary>
+    /// ステートクラス
+    /// </summary>
+    [RequireComponent(typeof(StateMachine))]
+    public abstract class State : MonoBehaviour
+    {
+        #region variable
+
+        /// <summary>
+        /// ステートマシン
+        /// </summary>
+        public StateMachine StateMachine { get; set; }
+
+        /// <summary>
+        /// ステート開始ストリーム
+        /// </summary>
+        private Subject<Unit> _begin = new Subject<Unit>();
+
+        /// <summary>
+        /// ステート終了ストリーム
+        /// </summary>
+        private Subject<Unit> _end = new Subject<Unit>();
+
+        #endregion
+
+        #region event stream
+
+        /// <summary>
+        /// ステート開始ストリーム
+        /// </summary>
+        public IObservable<Unit> BeginStream { get { return _begin.AsObservable(); } }
+
+        /// <summary>
+        /// ステート終了ストリーム
+        /// </summary>
+        public IObservable<Unit> EndStream { get { return _end.AsObservable(); } }
+
+        /// <summary>
+        /// Updateストリーム
+        /// </summary>
+        public IObservable<Unit> UpdateStream
+        {
+            get { return StateStream(this.UpdateAsObservable()); }
+        }
+
+        /// <summary>
+        /// FixedUpdateストリーム
+        /// </summary>
+        public IObservable<Unit> FixedUpdateStream
+        {
+            get { return StateStream(this.FixedUpdateAsObservable()); }
+        }
+
+        /// <summary>
+        /// LateUpdateストリーム
+        /// </summary>
+        public IObservable<Unit> LateUpdateStream
+        {
+            get { return StateStream(this.LateUpdateAsObservable()); }
+        }
+
+        /// <summary>
+        /// OnDrawGizmosストリーム
+        /// </summary>
+        public IObservable<Unit> OnDrawGizmosStream
+        {
+            get { return StateStream(this.OnDrawGizmosAsObservable()); }
+        }
+
+        /// <summary>
+        /// OnGUIストリーム
+        /// </summary>
+        public IObservable<Unit> OnGUIStream
+        {
+            get { return StateStream(this.OnGUIAsObservable()); }
+        }
+
+        /// <summary>
+        /// このステートが現在ステートの間だけメッセージが流れるストリーム
+        /// </summary>
+        protected IObservable<T> StateStream<T>(IObservable<T> source)
+        {
+            return source
+                // BeginStreamがOnNextされてから
+                .SkipUntil(BeginStream)
+                // EndStreamがOnNextされるまで
+                .TakeUntil(EndStream)
+                .RepeatUntilDestroy(gameObject)
+                .Publish()
+                .RefCount();
+        }
+
+        #endregion
+
+        #region method
+
+        /// <summary>
+        /// ステートの開始通知(StateMachine以外では基本的に呼び出さない)
+        /// </summary>
+        public void StateBegin()
+        {
+            _begin.OnNext(default(Unit));
+        }
+
+        /// <summary>
+        /// ステートの終了通知(StateMachine以外では基本的に呼び出さない)
+        /// </summary>
+        public void StateEnd()
+        {
+            _end.OnNext(default(Unit));
+        }
+
+        /// <summary>
+        /// ステートの遷移予約
+        /// </summary>
+        protected void Transition<T>() where T : State
+        {
+            StateMachine.Transition<T>();
+        }
+
+        #endregion
+    }
+}
---
+++ state-machine/Assets/Scripts/Framework/AI/StateMachine.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using UniRx;
+using UniRx.Triggers;
+
+
+namespace HC.AI
+{
+    /// <summary>
+    /// ステートマシン
+    /// </summary>
+    public class StateMachine : MonoBehaviour
+    {
+        #region variable
+
+        /// <summary>
+        /// ステートのマップ
+        /// </summary>
+        private Dictionary<Type, State> _stateMap = new Dictionary<Type, State>();
+
+        /// <summary>
+        /// 現在ステート
+        /// </summary>
+        [SerializeField, Tooltip("初期ステートをここに設定")]
+        private State _currentState = null;
+
+        /// <summary>
+        /// 遷移先ステート
+        /// </summary>
+        private Type _nextState = null;
+
+        #endregion
+
+        #region property
+
+        public State CurrentState
+        {
+            get { return _currentState; }
+        }
+
+        #endregion
+
+        #region event
+
+        private void Awake()
+        {
+            foreach (var state in GetComponents<State>())
+            {
+                Register(state);
+            }
+        }
+
+        private void Start()
+        {
+            // 最初のステートを開始
+            _currentState.StateBegin();
+
+            // 遷移先ステートが存在するならば遷移処理を行う
+            this.LateUpdateAsObservable()
+                .Where(_ => _nextState != null)
+                .Subscribe(_ =>
+                {
+                    // 現在ステートを終了
+                    _currentState.StateEnd();
+
+                    // 次ステートを開始
+                    _currentState = _stateMap[_nextState];
+                    _currentState.StateBegin();
+
+                    // 遷移先をnullで初期化
+                    _nextState = null;
+                });
+        }
+
+        #endregion
+
+        #region method
+
+        /// <summary>
+        /// ステートの登録
+        /// </summary>
+        /// <param name="state">登録するステート</param>
+        public void Register(State state)
+        {
+            _stateMap.Add(state.GetType(), state);
+            state.StateMachine = this;
+        }
+
+        /// <summary>
+        /// ステートの遷移予約
+        /// </summary>
+        public void Transition<T>() where T : State
+        {
+            _nextState = typeof(T);
+        }
+
+        #endregion
+    }
+}
