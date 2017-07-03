27902df3770f13a8ad39134994eb780f8c59792f
mori_akito <tomori.aki2961@gmail.com>
2017-07-04 00:14:37 +0900

    各ステートの処理のnamespaceからHCを削除

--- state-machine/Assets/Scripts/Example/IdleState.cs
+++ state-machine/Assets/Scripts/Example/IdleState.cs
@@ -4,58 +4,59 @@ using UniRx;
 using HC.AI;
 
 
-namespace HC
+[DisallowMultipleComponent]
+public class IdleState : State
 {
-    [DisallowMultipleComponent]
-    public class IdleState : State
+    #region variable
+
+    [Header("Parameter")]
+
+    /// <summary>
+    /// アピールステートに遷移する時間
+    /// </summary>
+    public const float TRANSITION_TO_APPEAL_DURATION = 3f;
+
+
+    [Header("Cache")]
+
+    [SerializeField]
+    private Animator _animator = null;
+
+    [SerializeField]
+    private Text _tutorialText = null;
+
+    #endregion
+
+    #region event
+
+    private void Reset()
     {
-        #region variable
-
-        [Header("Parameter")]
-
-        /// <summary>
-        /// アピールステートに遷移する時間
-        /// </summary>
-        public const float TRANSITION_TO_APPEAL_DURATION = 3f;
-
-
-        [Header("Cache")] [SerializeField] private Animator _animator = null;
-
-        [SerializeField] private Text _tutorialText = null;
-
-        #endregion
-
-        #region event
-
-        private void Reset()
-        {
-            _animator = transform.GetComponent<Animator>();
-        }
-
-        private void Start()
-        {
-            // 待機アニメーションを再生する
-            BeginStream.Subscribe(_ => _animator.Play("Idle"));
-
-            // チュートリアルの文言を変更する
-            BeginStream.Subscribe(_ => _tutorialText.text = "左クリックで走行ステートに遷移します");
-
-
-            float counter = 0f;
-
-            // n秒経過したらアピールステートに遷移する
-            UpdateStream.Do(_ => counter += Time.deltaTime)
-                .Where(count => counter > TRANSITION_TO_APPEAL_DURATION)
-                .Subscribe(_ => Transition<AppealState>());
-
-            // 右クリックされたら走行ステートに遷移する
-            UpdateStream.Where(_ => Input.GetMouseButtonDown(0))
-                .Subscribe(_ => Transition<RunState>());
-
-            // ステート遷移を行うときカウンタをリセットする
-            EndStream.Subscribe(_ => counter = 0f);
-        }
-
-        #endregion
+        _animator = transform.GetComponent<Animator>();
     }
+
+    private void Start()
+    {
+        // 待機アニメーションを再生する
+        BeginStream.Subscribe(_ => _animator.Play("Idle"));
+
+        // チュートリアルの文言を変更する
+        BeginStream.Subscribe(_ => _tutorialText.text = "左クリックで走行ステートに遷移します");
+
+
+        float counter = 0f;
+
+        // n秒経過したらアピールステートに遷移する
+        UpdateStream.Do(_ => counter += Time.deltaTime)
+            .Where(count => counter > TRANSITION_TO_APPEAL_DURATION)
+            .Subscribe(_ => Transition<AppealState>());
+
+        // 右クリックされたら走行ステートに遷移する
+        UpdateStream.Where(_ => Input.GetMouseButtonDown(0))
+            .Subscribe(_ => Transition<RunState>());
+
+        // ステート遷移を行うときカウンタをリセットする
+        EndStream.Subscribe(_ => counter = 0f);
+    }
+
+    #endregion
 }
--- state-machine/Assets/Scripts/Example/RunState.cs
+++ state-machine/Assets/Scripts/Example/RunState.cs
@@ -4,39 +4,40 @@ using UniRx;
 using HC.AI;
 
 
-namespace HC
+[DisallowMultipleComponent]
+public class RunState : State
 {
-    [DisallowMultipleComponent]
-    public class RunState : State
+    #region variable
+
+    [Header("Cache")]
+
+    [SerializeField]
+    private Animator _animator = null;
+
+    [SerializeField]
+    private Text _tutorialText = null;
+
+    #endregion
+
+    #region event
+
+    private void Reset()
     {
-        #region variable
-
-        [Header("Cache")] [SerializeField] private Animator _animator = null;
-
-        [SerializeField] private Text _tutorialText = null;
-
-        #endregion
-
-        #region event
-
-        private void Reset()
-        {
-            _animator = transform.GetComponent<Animator>();
-        }
-
-        private void Start()
-        {
-            // 走行アニメーションを再生する
-            BeginStream.Subscribe(_ => _animator.Play("Run"));
-
-            // チュートリアルの文言を変更する
-            BeginStream.Subscribe(_ => _tutorialText.text = "左クリックで待機ステートに遷移します");
-
-            // 右クリックされたら待機ステートに遷移する
-            UpdateStream.Where(_ => Input.GetMouseButtonDown(0))
-                .Subscribe(_ => Transition<IdleState>());
-        }
-
-        #endregion
+        _animator = transform.GetComponent<Animator>();
     }
+
+    private void Start()
+    {
+        // 走行アニメーションを再生する
+        BeginStream.Subscribe(_ => _animator.Play("Run"));
+
+        // チュートリアルの文言を変更する
+        BeginStream.Subscribe(_ => _tutorialText.text = "左クリックで待機ステートに遷移します");
+
+        // 右クリックされたら待機ステートに遷移する
+        UpdateStream.Where(_ => Input.GetMouseButtonDown(0))
+            .Subscribe(_ => Transition<IdleState>());
+    }
+
+    #endregion
 }
