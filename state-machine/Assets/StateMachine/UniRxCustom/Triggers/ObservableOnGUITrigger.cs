03e4780acef9679f584cd9c304ca0083011c9df9
tomori_aki <tomori.aki2961@gmail.com>
2018-02-28 18:18:07 +0900

    フォルダ構成の変更

---
+++ state-machine/Assets/StateMachine/UniRxCustom/Triggers/ObservableOnGUITrigger.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+
+namespace HC.UniRxCustom
+{
+    [DisallowMultipleComponent]
+    public class ObservableOnGUITrigger : ObservableTriggerBase
+    {
+        #region variable
+
+        private Subject<Unit> _onGui;
+
+        #endregion
+
+        #region property
+
+        public IObservable<Unit> OnGUIAsObservable()
+        {
+            return _onGui ?? (_onGui = new Subject<Unit>());
+        }
+
+        #endregion
+
+        #region event
+
+        private void OnGUI()
+        {
+            if (_onGui != null)
+            {
+                _onGui.OnNext(default(Unit));
+            }
+        }
+
+        #endregion
+
+
+
+        #region method
+
+        protected override void RaiseOnCompletedOnDestroy()
+        {
+            if (_onGui != null)
+            {
+                _onGui.OnCompleted();
+            }
+        }
+
+        #endregion
+    }
+}
